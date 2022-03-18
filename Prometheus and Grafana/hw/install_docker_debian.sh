e1564af70cec1ab5e1c9a06931af4a3c15d0368c
ivelin1936 <ivelin_1936@abv.bg>
2022-03-18 16:18:23 +0200

    comit

--- Prometheus and Grafana/hw/install_docker_debian.sh
+++ Prometheus and Grafana/hw/install_docker_debian.sh
@@ -11,14 +11,29 @@ echo "### Add Docker repository"
 # ID=${awk -F= '$1=="ID" { print $2 ;}' /etc/os-release}
 ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 VERSION=$(grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"')
+ARCH=$(dpkg --print-architecture)
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$ID $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$ID $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo add-apt-repository "deb [arch=$ARCH] https://download.docker.com/linux/$ID $(lsb_release -cs) stable"
+
 
 echo "Make sure you are about to install from the Docker repo instead of the default Ubuntu repo"
 apt-cache policy docker-ce
 
 echo "* Install Docker"
-apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
+# apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce
 
 echo "* Adjust group membership"
-usermod -aG docker vagrant
+sudo usermod -aG docker vagrant
+
+sudo systemctl restart docker
+echo "### Checking docker deamon status..."
+while true ; do
+    sleep 5
+    if [ "$(systemctl is-active docker)" = "active" ]; then
+        echo "Docker deamon is up! Checking status..."
+        systemctl status docker
+        break
+    fi
+done
