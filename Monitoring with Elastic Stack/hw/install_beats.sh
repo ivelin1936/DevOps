b34a0efc9533d97755df6158194a97c58eda2a90
ivelin1936 <ivelin_1936@abv.bg>
2022-03-26 17:04:02 +0200

    improved

---
+++ Monitoring with Elastic Stack/hw/install_beats.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#!/bin/bash
+
+HOSTNAME_IP=$(hostname -i | awk '{print $2}')
+echo "### Installing beats on host $HOSTNAME_IP..."
+
+DIR=${PWD}/beats
+[ -d "$DIR" ] || echo "Shared dir '$DIR' not found"
+
+METRICBEAT="$DIR/metricbeat_centos_setup.sh"
+CUSTOM_METRICBEAT_YML="$DIR/metricbeat.yml"
+ORIGINAL_YML_CONF="/etc/metricbeat/metricbeat.yml"
+
+[ -f "$METRICBEAT" ] && echo "Installing metricbeat:::..." && source $METRICBEAT || echo "File '$METRICBEAT' not found. Current dir: '${PWD}'"
+if [ -f $CUSTOM_METRICBEAT_YML ]; then
+    echo "::::: Custom configuration file '$CUSTOM_METRICBEAT_YML' detected!"
+    echo "Applying custom configuration..."
+    sudo cp -f $CUSTOM_METRICBEAT_YML $ORIGINAL_YML_CONF
+else 
+    echo "Not found custom configuration file '$CUSTOM_METRICBEAT_YML'."
+fi
