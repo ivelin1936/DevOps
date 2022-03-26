29601bf6aa11add02909cab69187dd72bdc00e77
ivelin1936 <ivelin_1936@abv.bg>
2022-03-26 16:01:10 +0200

    improved

--- Monitoring with Elastic Stack/hw/deploy_elk.sh
+++ Monitoring with Elastic Stack/hw/deploy_elk.sh
@@ -3,17 +3,17 @@
 HOSTNAME_IP=$(hostname -i | awk '{print $2}')
 echo "### Deploying ELK on host $HOSTNAME_IP..."
 
-SHARED_FOLDER=/elk
+SHARED_FOLDER=${PWD}/elk
 DOCKER_COMPOSE_FILE=docker-compose.yml
 test -f $SHARED_FOLDER/$DOCKER_COMPOSE_FILE || DOCKER_COMPOSE_FILE=docker-compose.yaml
 
 if [ -f $SHARED_FOLDER/$DOCKER_COMPOSE_FILE ]; then
     echo "::::: '$DOCKER_COMPOSE_FILE' file found... Deploying ELK..."
-    cp $SHARED_FOLDER/$DOCKER_COMPOSE_FILE . && sudo chown vagrant:vagrant $DOCKER_COMPOSE_FILE
-    docker compose up -d
+#     cp $SHARED_FOLDER/$DOCKER_COMPOSE_FILE . && sudo chown vagrant:vagrant $DOCKER_COMPOSE_FILE
+    docker-compose -f $SHARED_FOLDER/$DOCKER_COMPOSE_FILE up -d
 
     [ -z "$HOSTNAME_IP" ] && HOSTNAME_IP=$(hostname -i | awk '{print $1}')
-    echo "ELK deployed sucessfully! You can check Elasticserch: http://$HOSTNAME_IP:9200, Kibana: http://$HOSTNAME_IP:5601"
+    echo "ELK deployed sucessfully! You can check Kibana on http://$HOSTNAME_IP:5601"
 else 
     echo "There is no docker compose yaml file found at destination '$SHARED_FOLDER/$DOCKER_COMPOSE_FILE'...."
 fi
--- Monitoring with Elastic Stack/hw/metricbeat_config.sh
+++ Monitoring with Elastic Stack/hw/metricbeat_config.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-echo "### Installing metricbeat..."
-wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.1.0-x86_64.rpm
-sudo rpm -Uvh metricbeat-8.1.0-x86_64.rpm
+echo "### METRICBEAT CONFIG NOT IMPLEMENTED!!!!!!!"
+
+#TODO:
+# Now that we have it installed, we must configure it
+# Open the main configuration file
+# sudo vi /etc/metricbeat/metricbeat.yml
+# Change the following
+# Disable the Elasticsearch output (line #92) and enable the Logstash output (line #105)
+# Don’t forget to substitute the localhost with the IP of the Logstash server (192.168.99.101)
+# Save and close the file
+# Test the configuration with
+# sudo metricbeat test config
+# To check what modules are available, you can either
+# ls -al /etc/metricbeat/modules.d
+# Or execute the following
+# sudo metricbeat modules list
+# Now, if we want to enable a module, we can execute
+# sudo metricbeat modules enable system
+# This one may be enabled by default
+# Install the beat’s template in Elasticsearch
+# sudo metricbeat setup --index-management -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["192.168.99.101:9200"]'
+# Start and enable the Metricbeat service
+# sudo systemctl daemon-reload
+# sudo systemctl enable metricbeat
+# sudo systemctl start metricbeat
+
+# Repeat the same steps (without the template installation) on node 2 (node2) and check the results in Kibana UI
