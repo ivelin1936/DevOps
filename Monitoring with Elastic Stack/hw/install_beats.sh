5388df8e9a69d2f6de99ab9d107f9ebff3cb030b
ivelin1936 <ivelin_1936@abv.bg>
2022-03-26 17:10:11 +0200

    improved

--- Monitoring with Elastic Stack/hw/install_beats.sh
+++ Monitoring with Elastic Stack/hw/install_beats.sh
@@ -2,21 +2,27 @@
 
 #!/bin/bash
 
+DIR=${PWD}/beats
 HOSTNAME_IP=$(hostname -i | awk '{print $2}')
 echo "### Installing beats on host $HOSTNAME_IP..."
 
-DIR=${PWD}/beats
-[ -d "$DIR" ] || echo "Shared dir '$DIR' not found"
+metricbeat() {
+    METRICBEAT="$DIR/metricbeat_centos_setup.sh"
+    CUSTOM_METRICBEAT_YML="$DIR/metricbeat.yml"
+    ORIGINAL_YML_CONF="/etc/metricbeat/metricbeat.yml"
 
-METRICBEAT="$DIR/metricbeat_centos_setup.sh"
-CUSTOM_METRICBEAT_YML="$DIR/metricbeat.yml"
-ORIGINAL_YML_CONF="/etc/metricbeat/metricbeat.yml"
+    [ -f "$METRICBEAT" ] && echo "Installing metricbeat:::..." && source $METRICBEAT || echo "File '$METRICBEAT' not found. Current dir: '${PWD}'"
+    if [ -f $CUSTOM_METRICBEAT_YML ]; then
+        echo "::::: Custom configuration file '$CUSTOM_METRICBEAT_YML' detected!"
+        echo "Applying custom configuration..."
+        sudo cp -f $CUSTOM_METRICBEAT_YML $ORIGINAL_YML_CONF
+    else 
+        echo "Not found custom configuration file '$CUSTOM_METRICBEAT_YML'."
+    fi
+}
 
-[ -f "$METRICBEAT" ] && echo "Installing metricbeat:::..." && source $METRICBEAT || echo "File '$METRICBEAT' not found. Current dir: '${PWD}'"
-if [ -f $CUSTOM_METRICBEAT_YML ]; then
-    echo "::::: Custom configuration file '$CUSTOM_METRICBEAT_YML' detected!"
-    echo "Applying custom configuration..."
-    sudo cp -f $CUSTOM_METRICBEAT_YML $ORIGINAL_YML_CONF
-else 
-    echo "Not found custom configuration file '$CUSTOM_METRICBEAT_YML'."
-fi
+applyBeats() {
+    metricbeat
+}
+
+[ -d "$DIR" ] && applyBeats|| echo "Shared dir '$DIR' not found"
