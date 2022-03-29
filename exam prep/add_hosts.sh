c6fc3e67f0392d112aa991fb1ba7578f825875a8
ivelin1936 <ivelin_1936@abv.bg>
2022-03-29 21:00:52 +0300

    added needed scripts

---
+++ exam prep/add_hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "### Adding hosts..."
+echo "192.168.150.100 vm100.do1.exam jenkins" >> /etc/hosts
+echo "192.168.150.101 vm101.do1.exam docker" >> /etc/hosts
+echo "192.168.150.102 vm102.do1.exam promgraf" >> /etc/hosts
---
+++ exam prep/docker/add_user_do.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ $(id -u) -eq 0 ]; then
+	USERNAME="jenkins"
+	PASSWORD="Password1"
+	egrep "^$USERNAME" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$USERNAME exists!"
+		exit 1
+	else
+		useradd -m -p "$PASSWORD" "$USERNAME"
+		[ $? -eq 0 ] && echo "User has been added!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
+fi 
+sudo chpasswd <<<"$USERNAME:$PASSWORD"
+
+echo "### Adding the $USERNAME user to the sudoers list..."
+sudo sh -c "echo \"$USERNAME ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
+
+echo "### Adding the $USERNAME user to the docker group..."
+sudo usermod -aG docker jenkins
+#sudo usermod -aG root jenkins
+
+sudo systemctl restart docker
+echo "### Checking docker deamon status..."
+while true ; do
+    sleep 5
+    if [ "$(systemctl is-active docker)" = "active" ]; then 
+        echo "Docker deamon is up! Checking status..."
+        systemctl status docker
+        break
+    fi
+done
