8f99bedc5bdb41bcce100ec08f1b15b41a1edac9
ivelin1936 <ivelin_1936@abv.bg>
2022-03-30 10:19:53 +0300

    add hosts script

--- exam prep/add_hosts.sh
+++ exam prep/add_hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "### Adding hosts..."
-echo "192.168.150.100 vm100.do1.exam jenkins" >> /etc/hosts
-echo "192.168.150.101 vm101.do1.exam docker" >> /etc/hosts
-echo "192.168.150.102 vm102.do1.exam promgraf" >> /etc/hosts
+echo "192.168.56.11 vm100.do1.exam jenkins" >> /etc/hosts
+echo "192.168.56.12 vm101.do1.exam docker" >> /etc/hosts
+echo "192.168.56.13 vm102.do1.exam promgraf" >> /etc/hosts
